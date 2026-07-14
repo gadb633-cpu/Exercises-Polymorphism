c56b291f12ba1f3abe801dbb4a6945e8c6893554
gad-biton <gadb633@gmail.com>
2026-07-14 16:35:37 +0300

    added 8. Energy Usage Override

--- Main_Exercises.py
+++ Main_Exercises.py
@@ -119,74 +119,115 @@
 # print(lg.set_volume(8))
 # print(lg.set_volume(0))
 
-# 6. Uniform Command Execution
+# # 6. Uniform Command Execution
+# class Device:
+#     def __init__(self,name):
+#         self.name = name
+#     def run_command(self,cmd):
+#         return f"Device {self.name} received command: {cmd}."
+#     def send_command(self,device, cmd):
+#         return device.run_command(cmd)
+# class SmartTv(Device):
+#     def __init__(self, name):
+#         super().__init__(name)
+#     def run_command(self,cmd):
+#         return f"Device {self.name} is: {cmd}."    
+# class SmartSpeaker(Device):
+#     def __init__(self, name):
+#         super().__init__(name)
+#     def run_command(self,cmd):
+#         return f"Device {self.name} Low battery: {cmd}."
+# class SmartLamp(Device):
+#     def __init__(self, name):
+#         super().__init__(name)
+#     def run_command(self,cmd):
+#         return f"Device {self.name} Low light: {cmd}."          
+# class SmartAC(Device):
+#     def __init__(self, name):
+#         super().__init__(name)
+#     def run_command(self,cmd):
+#         return f"Device {self.name} is No refrigerator: {cmd}."  
+# list_instance =[]
+# lg = SmartTv("LG")
+# desk_Lamp = SmartLamp("Desk Lamp")    
+# echo = SmartSpeaker("Echo")
+# samsung = SmartAC("samsung")
+# list_instance.append(lg)
+# list_instance.append(desk_Lamp)                      
+# list_instance.append(echo)                      
+# list_instance.append(samsung)
+# for list1 in list_instance:
+#     print(list1.send_command(list1,"start"))
+
+# # 7. Smart Home Schedule
+# class Device:
+#     def __init__(self,name):
+#         self.name =name
+#     def run_schedule(self,hour):
+#         return f"{self.name} is idle."
+# class SmartLamp(Device):
+#     def __init__(self, name):
+#         super().__init__(name)
+#     def run_schedule(self,hour):
+#         return f"{self.name}: turning on." if 18 <= hour <= 23 else f"{self.name}: turning off."
+# class SmartAC(Device):
+#     def __init__(self, name):
+#         super().__init__(name)
+#     def run_schedule(self,hour):
+#         return f"{self.name}: turning on." if 12 <= hour <= 20 else f"{self.name}: turning off."
+# class SmartTV(Device):
+#     def __init__(self, name):
+#         super().__init__(name)
+#     def run_schedule(self,hour):
+#         return f"{self.name}: turning on." if 20 <= hour <= 23 else f"{self.name}: turning off." 
+# desk_Lamp = SmartLamp("Desk Lamp")                                
+# samsung = SmartAC("samsung")
+# lg = SmartTV("LG")
+# print(desk_Lamp.run_schedule(21))
+# print(samsung.run_schedule(21))
+# print(lg.run_schedule(21))
+
+# 8. Energy Usage Override
 class Device:
-    def __init__(self,name):
+    def __init__(self,name,watts):
         self.name = name
-    def run_command(self,cmd):
-        return f"Device {self.name} received command: {cmd}."
-    def send_command(self,device, cmd):
-        return device.run_command(cmd)
+        self.watts=watts
+    def energy_usage(self):
+        return self.watts
 class SmartTv(Device):
-    def __init__(self, name):
-        super().__init__(name)
-    def run_command(self,cmd):
-        return f"Device {self.name} is: {cmd}."    
+    def __init__(self, name,watts):
+        super().__init__(name,watts)
+    def energy_usage(self):
+        return f"{self.name}:{self.watts}w"    
 class SmartSpeaker(Device):
-    def __init__(self, name):
-        super().__init__(name)
-    def run_command(self,cmd):
-        return f"Device {self.name} Low battery: {cmd}."
+    def __init__(self, name,watts):
+        super().__init__(name,watts)
+    def energy_usage(self):
+        return f"{self.name}:{self.watts}w"
 class SmartLamp(Device):
-    def __init__(self, name):
-        super().__init__(name)
-    def run_command(self,cmd):
-        return f"Device {self.name} Low light: {cmd}."          
+    def __init__(self, name,watts):
+        super().__init__(name,watts)
+    def energy_usage(self):
+        return f"{self.name}:{self.watts}w"        
 class SmartAC(Device):
-    def __init__(self, name):
-        super().__init__(name)
-    def run_command(self,cmd):
-        return f"Device {self.name} is No refrigerator: {cmd}."  
+    def __init__(self, name,watts):
+        super().__init__(name,watts)
+    def energy_usage(self):
+        return f"{self.name}:{self.watts}w" 
 list_instance =[]
-lg = SmartTv("LG")
-desk_Lamp = SmartLamp("Desk Lamp")    
-echo = SmartSpeaker("Echo")
-samsung = SmartAC("samsung")
+lg = SmartTv("LG",150)
+desk_Lamp = SmartLamp("Desk Lamp",8)    
+echo = SmartSpeaker("Echo",30)
+samsung = SmartAC("samsung",900)
 list_instance.append(lg)
 list_instance.append(desk_Lamp)                      
 list_instance.append(echo)                      
 list_instance.append(samsung)
-for list1 in list_instance:
-    print(list1.send_command(list1,"start"))
-
-# 7. Smart Home Schedule
-class Device:
-    def __init__(self,name):
-        self.name =name
-    def run_schedule(self,hour):
-        return f"{self.name} is idle."
-class SmartLamp(Device):
-    def __init__(self, name):
-        super().__init__(name)
-    def run_schedule(self,hour):
-        return f"{self.name}: turning on." if 18 <= hour <= 23 else f"{self.name}: turning off."
-class SmartAC(Device):
-    def __init__(self, name):
-        super().__init__(name)
-    def run_schedule(self,hour):
-        return f"{self.name}: turning on." if 12 <= hour <= 20 else f"{self.name}: turning off."
-class SmartTV(Device):
-    def __init__(self, name):
-        super().__init__(name)
-    def run_schedule(self,hour):
-        return f"{self.name}: turning on." if 20 <= hour <= 23 else f"{self.name}: turning off." 
-desk_Lamp = SmartLamp("Desk Lamp")                                
-samsung = SmartAC("samsung")
-lg = SmartTV("LG")
-print(desk_Lamp.run_schedule(21))
-print(samsung.run_schedule(21))
-print(lg.run_schedule(21))
-
+total = 0
+for instance in list_instance:
+    total += instance.watts
+    print(instance.energy_usage())
+print(f"total is: {total}")
 
 
 
