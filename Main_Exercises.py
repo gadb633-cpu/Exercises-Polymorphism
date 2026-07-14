2b8b5702d9f5b82157948a52492a712f3a06f422
gad-biton <gadb633@gmail.com>
2026-07-14 12:14:13 +0300

    added 2. Deactivate with Override

--- Main_Exercises.py
+++ Main_Exercises.py
@@ -18,3 +18,25 @@ samsung = SmartTV("Samsung")
 echo = SmartSpeaker("Echo")
 print(samsung.activate())
 print(echo.activate())
+
+# 2. Deactivate with Override
+class Device:
+    def __init__(self,name):
+        self.name = name
+    def deactivate(self):
+        return f"Device {self.name} is now off."
+class SmartLamp(Device):
+    def __init__(self, name):
+        super().__init__(name)
+    def deactivate(self):
+        return f"Lamp {self.name} is dimming and turning off."
+class SmartAC(Device):
+    def __init__(self, name):
+        super().__init__(name)
+    def deactivate(self):
+        return f"AC {self.name} is cooling down and switching off."     
+Bedroom_Lamp=SmartLamp("Bedroom Lamp")
+Living_Room_AC=SmartAC("Living Room AC")
+print(Bedroom_Lamp.deactivate())
+print(Living_Room_AC.deactivate())
+    
