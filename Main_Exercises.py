746bc457498ea1308c4b7659f1758ef181f2a53f
gad-biton <gadb633@gmail.com>
2026-07-14 14:18:55 +0300

    added 4. Mixed List Activation

--- Main_Exercises.py
+++ Main_Exercises.py
@@ -64,4 +64,39 @@ lg = SmartTV("LG", True, 8)
 alexa = SmartSpeaker("Alexa", True, "Bohemian Rhapsody")         
 print(lg.status())
 print(alexa.status())
-    
+
+# 4. Mixed List Activation
+class Device:
+    def __init__(self,name):
+        self.name =name
+    def activate(self):
+        return f"Device {self.name} is now on."
+class SmartTV(Device):
+    def __init__(self, name):
+        super().__init__(name)
+    def activate(self):
+        return f"{self.name} is playing the home screen."
+class SmartLamp(Device):
+    def __init__(self, name):
+        super().__init__(name)
+    def activate(self):
+        return f"{self.name} is glowing warmly."
+class SmartSpeaker(Device):
+    def __init__(self, name):
+        super().__init__(name)
+    def activate(self):
+        return f"Speaker {self.name} is ready to play musi."
+list_instance = []
+lg = SmartTV("LG")
+desk_Lamp = SmartLamp("Desk Lamp")    
+echo = SmartSpeaker("Echo")
+list_instance.append(lg)
+list_instance.append(desk_Lamp)
+list_instance.append(echo)
+for list1 in list_instance:
+    print(list1.activate())
+
+
+
+
+
